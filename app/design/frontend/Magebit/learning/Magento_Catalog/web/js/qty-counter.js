630ed486936dbdae5e1c9a9bb2101fb0a53f08b3
IvansMuzicenkoMagebit <ivans.muzicenko@magebit.com>
2022-09-30 14:25:39 +0300

    Development task 'ML01: Product Page Styling' Knockout.js "add to cart" qty change

--- app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
+++ app/design/frontend/Magebit/learning/Magento_Catalog/web/js/qty-counter.js
@@ -1,2 +1,34 @@
-const productCounter = document.querySelector("#qty");
+define([
+    'ko',
+    'uiElement'
+], function (ko, Element) {
+    return Element.extend({
+        defaults: {
+            template: 'Magento_Catalog/input-counter'
+        },
 
+        initObservable: function () {
+            this._super().observe('qty');
+
+            return this;
+        },
+
+        getDataValidator: function() {
+            return JSON.stringify(this.dataValidate);
+        },
+
+        decreaseQty: function() {
+            let qty;
+
+            this.qty() > 1 ? qty = this.qty() - 1 : qty = 1
+
+            this.qty(qty);
+        },
+
+        increaseQty: function() {
+            let qty = this.qty() + 1;
+
+            this.qty(qty);
+        }
+    });
+});
